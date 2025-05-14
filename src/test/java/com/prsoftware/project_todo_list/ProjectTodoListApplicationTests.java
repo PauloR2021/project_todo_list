d06a8e7a89a68947f946524b42971b253281a1dc
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-05-14 18:49:32 -0300

    Inicializando Projeto

---
+++ src/test/java/com/prsoftware/project_todo_list/ProjectTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prsoftware.project_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
